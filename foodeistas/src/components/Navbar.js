2f3efa141e9df1f9bd091cf8e9c7375136d04915
soumili202 <soumilirupsa2020@gmail.com>
2023-11-30 14:52:09 +0530

    Made navbar responsive

--- foodeistas/src/components/Navbar.js
+++ foodeistas/src/components/Navbar.js
@@ -5,25 +5,32 @@ import {FaBars, FaTimes} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './Navstyles.css';
 export const Navbar = () => {
-    const navref= useRef();
-    const showNav = () => {
-        navref.current.classList.toggle('nav-show');
-    }
-    return <header>
-    
-    <nav className="nav" ref={navref}>
-        <h1 className="logo">Foodie</h1>
-        
-        <ul>
-            <Link to="/" className="x"><li>Home</li></Link>
-            <Link to="/about" className="x"><li>About</li></Link>
-            <Link to="/restaurants" className="x"><li>Restaurants</li></Link>
-            <Link to="/contact" className="x"><li>Contact</li></Link>
-        </ul>
-        
-    </nav>
-    
-    </header>
+    const [isMobile, setIsMobile] = React.useState(false);
+    const [activeLink, setActiveLink] = React.useState('/');
 
-
+    return (
+        <header>
+            <nav className="nav">
+                <h1 className="logo">Foodie</h1>
+                <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+                
+                    <Link to="/" className={activeLink === '/' ? 'active' : 'x'} onClick={() => setActiveLink('/')}>
+                        <li>Home</li>
+                    </Link>
+                    <Link to="/about" className={activeLink === '/about' ? 'active' : 'x'} onClick={() => setActiveLink('/about')}>
+                        <li>About</li>
+                    </Link>
+                    <Link to="/restaurants" className={activeLink === '/restaurants' ? 'active' : 'x'} onClick={() => setActiveLink('/restaurants')}>
+                        <li>Restaurants</li>
+                    </Link>
+                    <Link to="/contact" className={activeLink === '/contact' ? 'active' : 'x'} onClick={() => setActiveLink('/contact')}>
+                        <li>Contact</li>
+                    </Link>
+                </ul>
+                <button className="mobile-menu" onClick={() => setIsMobile(!isMobile)}>
+                    {isMobile ? <FaTimes /> : <FaBars />}
+                </button>
+            </nav>
+        </header>
+    );
 }
